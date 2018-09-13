fe16719737bece014c2ed10b7b065a75a931da2e
Tim Z Liu <995332120@qq.com>
2018-09-13 17:49:57 +0800

    finishe

--- pages/calculator/calculator.js
+++ pages/calculator/calculator.js
@@ -1,11 +1,30 @@
 
+
+import rpn from '../../utils/rpn.js';
+
 Page({
   data: {
-
+    express: '',
+    caclu:'',
+    result:''
   },
   onLoad: function () {
     this.setData({
       
     })
+  },
+  express(event){
+    this.data.express += event.target.id;
+    console.log('this.data.express', this.data.express);
+    if (/^[\d|\+\-\*/\%\.]$/.test(event.target.id)){
+      this.data.caclu += event.target.id;
+    }
+    this.setData({ express: this.data.express,caclu:this.data.caclu});
+  },
+  caclu(){
+    // rpn.calculate(this.data.caclu);
+    let result = new rpn(this.data.caclu).calculate() 
+    console.log('result', result);
+    this.setData({ express: '', caclu: '', result: result});
   }
 })
