b5804708728135eedf1f169a785b7b54ac6f94a8
Tim Z Liu <995332120@qq.com>
2018-09-16 12:29:33 +0800

    adjust

--- backend/config/plugin.js
+++ backend/config/plugin.js
@@ -9,11 +9,3 @@ exports.io = {
   enable: true,
   package: 'egg-socket.io',
 };
-exports.redis = {
-  enable: false,
-  package: 'egg-redis',
-};
-exports.mongoose = {
-  enable: false,
-  package: 'egg-mongoose',
-};
