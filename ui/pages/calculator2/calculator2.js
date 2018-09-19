ca8a601bfb61e5e816b90fead8591c3d5e9bbd62
Tim Z Liu <995332120@qq.com>
2018-09-19 15:32:06 +0800

    add logic

--- ui/pages/calculator2/calculator2.js
+++ ui/pages/calculator2/calculator2.js
@@ -6,30 +6,49 @@ Page({
     caclu: '',
     tempCaclu: '',
     result: '',
-    express:''
+    express: ''
   },
   onLoad: function() {
     this.setData({
 
     })
     this.receiceResult();
+    console.log(this.addComma(1234.234)); 
+  },
+  addComma(num) {
+    num = num.toString();
+    let result = num.toString();
+    result = result.replace(/\.\d*$/, '');
+    let len = result.length / 3;
+    result = result.split('').reverse().join('');
+    console.log('result', result);
+    let j = 1;
+    for (let i = 0; i < result.length; i++) {
+      if ((i + 1) % 4 === 0) {
+        var tmp = result.substring(0, i);
+        var estr = result.substring(i, result.length);
+        result = tmp + ',' + estr;
+        i++;
+      }
+    }
+    console.log('resultResult', result);
+    console.log('num', num);
+    return num.replace(/^\d*/, result.split('').reverse().join(''));
   },
   receiceResult() {
     const that = this;
     const socket = socketService.getSocket();
     socket.on('calculator', (data) => {
       let result = data.result;
-      console.log('message', result);
-     
       if (result !== 0) {
         this.setData({
           express: this.data.result + '=',
           caclu: result || '',
-          result: result || ''
+          result: this.addComma(result) || ''
         });
       } else {
         this.setData({
-          express: this.data.result +'=',
+          express: this.data.result + '=',
           caclu: result,
           result: result
         });
@@ -37,13 +56,13 @@ Page({
     })
   },
 
-  dirtyClear(id){
-    if (/[\+\-\*\/]$/.test(id) && /[\+\-\*\/]$/.test(this.data.caclu) ){
+  dirtyClear(id) {
+    if (/[\+\-\*\/]$/.test(id) && /[\+\-\*\/]$/.test(this.data.caclu)) {
       this.data.caclu = this.data.caclu.toString();
       this.data.caclu = this.data.caclu.substring(0, this.data.caclu.length - 1);
-     return this.data.caclu; 
+      return this.data.caclu;
     };
-    return this.data.caclu ;
+    return this.data.caclu;
   },
 
   trans(express) {
@@ -69,46 +88,47 @@ Page({
   express(event) {
     this.data.caclu = this.dirtyClear(event.target.id) || '';
     this.data.caclu = this.data.caclu.toString();
-   
-      if (event.target.id === '(') {
-        event.target.id  = this.trans(this.data.caclu)
+
+    if (event.target.id === '(') {
+      event.target.id = this.trans(this.data.caclu)
     }
     if (event.target.id === '%') {
       this.data.caclu = this.cacluPercent(this.data.caclu)
     }
-    if (this.data.tempCaclu){
+    if (this.data.tempCaclu && event.target.id !== '(-') {
       this.setData({
-        tempCaclu:''
+        tempCaclu: ''
       });
     }
-     if (event.target.id === '(-') {
-       if (!this.data.tempCaclu){
-         this.data.tempCaclu =this.data.caclu 
-         if (!this.data.caclu) { this.data.caclu = event.target.id;} 
-         else if (/\d$/.test(this.data.caclu)) {
-           let num = this.data.caclu.match(/\d*\.?\d*$/)[0];
-           this.data.caclu = this.data.caclu.replace(/\d*\.?\d*$/, '');
-           this.data.caclu += event.target.id + num;
-         } else {
-           this.data.caclu += event.target.id;
-         }
-       }else{
-         this.data.caclu = this.data.tempCaclu;
-         this.data.tempCaclu = '';
-         this.setData({
-           tempCaclu: this.data.tempCaclu,
-           caclu: this.data.caclu
-         });
-       }
-     
+    if (event.target.id === '(-') {
+      if (!this.data.tempCaclu) {
+        this.data.tempCaclu = this.data.caclu
+        if (!this.data.caclu) {
+          this.data.caclu = event.target.id;
+        } else if (/\d$/.test(this.data.caclu)) {
+          let num = this.data.caclu.match(/\d*\.?\d*$/)[0];
+          this.data.caclu = this.data.caclu.replace(/\d*\.?\d*$/, '');
+          this.data.caclu += event.target.id + num;
+        } else {
+          this.data.caclu += event.target.id;
+        }
+      } else {
+        this.data.caclu = this.data.tempCaclu;
+        this.data.tempCaclu = '';
+        this.setData({
+          tempCaclu: this.data.tempCaclu,
+          caclu: this.data.caclu
+        });
+      }
+
     }
     if (/^[\d|\+\-\*/\.\(\)]$/.test(event.target.id)) {
       this.data.caclu += event.target.id;
     }
     this.data.result = this.data.caclu.replace(/\*/g, 'x');
     this.data.result = this.data.result.replace(/\//g, '÷');
-   
-    
+
+
     this.setData({
       caclu: this.data.caclu,
       result: this.data.result
